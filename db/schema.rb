d1d5380793a07090e749e2de1a56ca89ef98627c
virginianimd <madevirginia06@gmail.com>
2019-09-15 13:27:58 +0800

    [Add] Adding Field Member in Boards

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_031217) do
+ActiveRecord::Schema.define(version: 2019_09_15_052521) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_09_10_031217) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "member"
   end
 
   create_table "cards", force: :cascade do |t|
