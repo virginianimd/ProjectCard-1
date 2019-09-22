b4bcbb172e3ddc52cd7c4f2db2af516b3c8364c6
mpuchan <iputumpu23@gmail.com>
2019-09-22 11:56:37 +0800

    [update] schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_09_19_090404) do
-=======
 ActiveRecord::Schema.define(version: 2019_09_22_031501) do
->>>>>>> [Update] card
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
